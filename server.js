23b698715b37b4e3b90610cf3708751ce7e08e6c
nadjarben <nadjarbenj@gmail.com>
2019-12-08 16:08:55 +0200

    rm

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ app.use(cors())
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-  console.log('test')
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
