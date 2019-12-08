25d36391811b9a755558a2715e0d3f5221df07f3
nadjarben <nadjarbenj@gmail.com>
2019-12-08 15:15:32 +0200

    i am

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors())
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    //next();
     });
 
   // Use Routes
